005cc0d0b269057d81484a147868ba1aa213aac0
Carlos Solano <carlos.solanogomez@hotmail.com>
2021-02-21 08:50:47 +0100

    Commit modificacion

--- scrips.js
+++ scrips.js
@@ -1,16 +1,20 @@
 let palabras = ['café', 'croissant', 'tostada', 'zumo'];
-let palabrasParesModificadas = modificaPares(palabras, elemento => elemento.charAt(0).toUpperCase() + elemento.slice(1));
-console.log(palabrasParesModificadas);
+
 
 function modificaPares(palabras, fx) {
   let palabrasParesModificadas = [];
   for (const i in palabras) {
     if (i % 2 !== 0) {
-      palabrasParesModificadas.push(fx(palabras[indice]));
+      palabrasParesModificadas.push(fx(palabras));
     } else {
-      palabrasParesModificadas.push(palabras[indice]);
+      palabrasParesModificadas.push(palabras);
     }
 
   }
   return palabrasParesModificadas;
 }
+
+let palabrasParesModificadas = modificaPares(palabras, elemento => elemento.charAt(0).toUpperCase() + elemento.slice(1));
+
+
+console.log(palabrasParesModificadas);
